1b45c70d6337ac7948dfe1865fda20f8d30388f6
Martin <martingatski@gmail.com>
2019-09-12 19:03:30 +0200

    optimizing

--- script.js
+++ script.js
@@ -1,132 +1,139 @@
 let input = document.querySelector("#picker");
-const container = document.querySelectorAll("#container>div");
+const container = document.querySelectorAll(".box");
 const select = document.querySelector("select");
 let option = document.querySelector("option");
 let harmony = "analogous";
 
-input.addEventListener("input", getColors, false);
+
+initColors();
+
+function initColors() {
+    getColors();
+    input.addEventListener("input", getColors, false);
 
 
-function getColors() {
-    HEXtoRGB(input.value);
-}
+    function getColors() {
+        HEXtoRGB(input.value);
+    }
 
 
-function HEXtoRGB(h) {
-    let r = 0,
-        g = 0,
-        b = 0;
+    function HEXtoRGB(h) {
+        let r = 0,
+            g = 0,
+            b = 0;
 
-    r = "0x" + h[1] + h[2];
-    g = "0x" + h[3] + h[4];
-    b = "0x" + h[5] + h[6];
+        r = "0x" + h[1] + h[2];
+        g = "0x" + h[3] + h[4];
+        b = "0x" + h[5] + h[6];
 
-    convertRGBtoHSL(r, g, b);
-    return +r + "," + +g + "," + +b;
-}
+        RGBtoHSL(r, g, b);
+        return +r + "," + +g + "," + +b;
+    }
 
 
-function convertRGBtoHSL(r, g, b) {
+    function RGBtoHSL(r, g, b) {
 
-    r /= 255;
-    g /= 255;
-    b /= 255;
+        r /= 255;
+        g /= 255;
+        b /= 255;
 
-    let min = Math.min(r, g, b),
-        max = Math.max(r, g, b),
-        delta = max - min,
-        h = 0,
-        s = 0,
-        l = 0;
+        let min = Math.min(r, g, b),
+            max = Math.max(r, g, b),
+            delta = max - min,
+            h = 0,
+            s = 0,
+            l = 0;
 
-    if (delta == 0)
-        h = 0;
-    else
-    if (max == r)
-        h = ((g - b) / delta) % 6;
-    else
-    if (max == g)
-        h = (b - r) / delta + 2;
-    else
-        h = (r - g) / delta + 4;
-    h = Math.round(h * 60);
-    if (h < 0)
-        h += 360;
+        if (delta == 0)
+            h = 0;
+        else
+        if (max == r)
+            h = ((g - b) / delta) % 6;
+        else
+        if (max == g)
+            h = (b - r) / delta + 2;
+        else
+            h = (r - g) / delta + 4;
+        h = Math.round(h * 60);
+        if (h < 0)
+            h += 360;
 
-    l = (max + min) / 2;
+        l = (max + min) / 2;
 
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
 
-    calcHarmony(h, s, l);
-}
+        calcHarmony(h, s, l);
+    }
 
-select.onchange = function () {
-    harmony = select.value;
-    HEXtoRGB(input.value);
-}
 
-function calcHarmony(h, s, l) {
-    hBase = h;
-    sBase = s;
-    lBase = l;
-    let x = 0;
+    select.onchange = function () {
+        harmony = select.value;
+        HEXtoRGB(input.value);
+    }
 
-    for (i = -2; i < 3; i++) {
-        if (harmony == "analogous") {
-            h = hBase + (20 * i);
-        }
-        if (harmony == "monochromatic") {
-            l = (lBase + 15 * i).toFixed(1);
-        }
-        if (harmony == "triad") {
-            h = hBase + (30 * i);
-        }
-        if (harmony == "complementary") {
-            h = hBase + (60 * i);
-        }
-        if (harmony == "compound") {
-            if (i < 0) {
-                h = hBase + (60 * i);
-            }
-            if (i > 0) {
+    function calcHarmony(h, s, l) {
+        hBase = h;
+        sBase = s;
+        lBase = l;
+        let x = 0;
+
+        for (i = -2; i < 3; i++) {
+            if (harmony == "analogous") {
                 h = hBase + (20 * i);
             }
+            if (harmony == "monochromatic") {
+                l = (lBase + 15 * i).toFixed(1);
+            }
+            if (harmony == "triad") {
+                h = hBase + (30 * i);
+            }
+            if (harmony == "complementary") {
+                h = hBase + (60 * i);
+            }
+            if (harmony == "compound") {
+                if (i < 0) {
+                    h = hBase + (60 * i);
+                }
+                if (i > 0) {
+                    h = hBase + (20 * i);
+                }
+            }
+            if (harmony == "shades") {
+                s = (lBase + 20 * i).toFixed(1);
+            }
+
+            container[x].style.backgroundColor = `hsl(${h},${s}%,${l}%)`;
+            container[2].style.backgroundColor = `hsl(${hBase},${sBase}%,${lBase}%)`;
+            container[x].firstElementChild.lastElementChild.innerHTML = `HSL: ${h},${s}%,${l}%`;
+            let rgb = container[x].style.backgroundColor;
+
+            function setRGB(rgb) {
+                let sep = rgb.indexOf(", ") > -1 ? ", " : " ";
+                rgb = rgb.substr(4).split(")")[0].split(sep);
+                return rgb;
+            }
+
+            function RGBtoHex(rgb) {
+                let r = (+rgb[0]).toString(16),
+                    g = (+rgb[1]).toString(16),
+                    b = (+rgb[2]).toString(16);
+
+                if (r.length == 1)
+                    r = "0" + r;
+                if (g.length == 1)
+                    g = "0" + g;
+                if (b.length == 1)
+                    b = "0" + b;
+
+                return "#" + r + g + b;
+            }
+
+            container[x].firstElementChild.children[1].innerHTML = "RGB: " + setRGB(rgb);
+            container[x].firstElementChild.children[0].innerHTML = "HEX: " + RGBtoHex(setRGB(rgb));
+            x++;
         }
-        if (harmony == "shades") {
-            s = (lBase + 20 * i).toFixed(1);
-        }
-
-        container[x].style.backgroundColor = `hsl(${h},${s}%,${l}%)`;
-        container[2].style.backgroundColor = `hsl(${hBase},${sBase}%,${lBase}%)`;
-        container[x].firstElementChild.lastElementChild.innerHTML = `HSL: ${h},${s}%,${l}%`;
-        let rgb = container[x].style.backgroundColor;
-
-        function setRGB(rgb) {
-            let sep = rgb.indexOf(", ") > -1 ? ", " : " ";
-            rgb = rgb.substr(4).split(")")[0].split(sep);
-            return rgb;
-        }
-
-        function convertRGBtoHex(rgb) {
-            let r = (+rgb[0]).toString(16),
-                g = (+rgb[1]).toString(16),
-                b = (+rgb[2]).toString(16);
-
-            if (r.length == 1)
-                r = "0" + r;
-            if (g.length == 1)
-                g = "0" + g;
-            if (b.length == 1)
-                b = "0" + b;
-
-            return "#" + r + g + b;
-        }
-
-        container[x].firstElementChild.children[1].innerHTML = "RGB: " + setRGB(rgb);
-        container[x].firstElementChild.children[0].innerHTML = "HEX: " + convertRGBtoHex(setRGB(rgb));
-        x++;
     }
 }
